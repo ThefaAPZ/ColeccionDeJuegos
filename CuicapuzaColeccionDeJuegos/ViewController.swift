1ec9becb9afd6e2b9e1e6bffc988f53503f466a7
Mireya ESthefany Apaza Cuicapuza <mireya.apaza@tecsup.edu.pe>
2024-05-08 11:37:38 -0500

    Initial Commit

---
+++ CuicapuzaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CuicapuzaColeccionDeJuegos
+//
+//  Created by Mireya Esthefany Apaza Cuicapuza  on 8/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
